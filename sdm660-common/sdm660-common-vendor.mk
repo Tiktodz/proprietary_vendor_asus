42e0360ff79606aee753e7b6cb312807ec528ad3
Anush02198 <Anush02198@users.noreply.github.com>
2021-10-21 13:03:34 +0530

    sdm660-common: Drop Datastatusnotification - Fixes Ril with Dual Sim on Android SnowCone Signed-off-by: nktn30 <nkwhitehat@gmail.com>
    Signed-off-by: Tiktodz <kuplemarkeple@gmail.com>

--- sdm660-common/sdm660-common-vendor.mk
+++ sdm660-common/sdm660-common-vendor.mk
@@ -583,7 +583,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
